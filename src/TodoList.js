846a3d06a280b6f9a5247712fe95608f5df30c0d
tangshiya <tsy17321289030@163.com>
2020-02-02 21:16:27 +0800

    添加和删除功能完成

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,35 +1,65 @@
 import React, { Component } from 'react'
-import { Input, Button, List } from 'antd'
+import { Input, Button } from 'antd'
 import 'antd/dist/antd.css'
 
-const data = [
-  '吃饭',
-  '睡觉',
-  '打豆豆'
-]
-
 class TodoList extends Component {
+
+  state = {
+    list: [
+      '吃饭',
+      '睡觉',
+      '打豆豆'
+    ],
+    inputValue: ''
+  }
+
   render() { 
+    const { list, inputValue } = this.state
     return ( 
       <div style={{ margin: "10px" }}>
         <Input 
           placeholder="write somthing"
+          value={inputValue}
+          onChange={this.handleClickChange}
           style={{ width: "300px", marginRight: '10px' }}
         />
         <Button
           type="primary"
+          onClick={this.handleClickAdd}
         >
           添加
         </Button>
-        <List
-          style={{ marginTop: '20px', width: '400px' }}
-          bordered
-          dataSource={data}
-          renderItem={ item => <List.Item>{item}</List.Item>}
-        >
-        </List>
+        <ul>
+          {
+            list.map(( item, index ) => {
+              return <li key={index} onClick={this.handleClickDelete.bind(this, index)}>{item}</li>
+            })
+          }
+        </ul>
       </div>
-     )
+    )
+  }
+
+  handleClickChange = e => {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  handleClickAdd = () => {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+
+  handleClickDelete = index => {
+    // state 不允许做任何改变
+    const list = [...this.state.list]
+    list.splice(index, 1)
+    this.setState({
+      list
+    })
   }
 }
  
